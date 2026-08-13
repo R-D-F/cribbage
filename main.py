a80938b1383a61440c7dd424c0e668a2b4987c3c
riley <rifrancis22@gmail.com>
2026-08-12 20:32:42 -0700

    some progress

--- main.py
+++ main.py
@@ -2,21 +2,84 @@ from dataclasses import dataclass
 from itertools import combinations
 from random import sample
 
+from ascii_cards import print_card
+
 
 @dataclass(frozen=True)
 class Card:
-    value: str
+    rank: str
     suit: str
+
+    @property
+    def value(self):
+        values = {
+            "A": 1,
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9,
+            "10": 10,
+            "J": 10,
+            "Q": 10,
+            "K": 10,
+        }
+        return values[self.rank]
+
+
+# class Card_2:
+#     def __init__(self, rank, suit):
+#         self.rank = rank
+#         self.suit = suit
+#         self.values = {
+#             "A": 1,
+#             "2": 2,
+#             "3": 3,
+#             "4": 4,
+#             "5": 5,
+#             "6": 6,
+#             "7": 7,
+#             "8": 8,
+#             "9": 9,
+#             "10": 10,
+#             "J": 10,
+#             "Q": 10,
+#             "K": 10,
+#         }
+#         self.value = self.values[self.rank]
+
+
+def print_hand(hand):
+    for c in hand:
+        print_card(c.rank, c.suit)
 
 
 def score_hand(hand: list[Card], discard: list[Card]):
-    deck = [Card(value, suit) for value in values for suit in suits]
+    max_score = 0
+    best_hand = []
+    deck = [Card(rank, suit) for rank in ranks for suit in suits]
     deck_without_hand = [card for card in deck if card not in hand]
-    return len(deck_without_hand)
+    hand = list(hand)
+    print(hand)
+    for card in deck_without_hand:
+        score = 0
+        hand.append(card)
+        print
+        for two_card_combo in combinations(hand, 2):
+            if two_card_combo[0].value + two_card_combo[1].value == 15:
+                score += 2
+        if score > max_score:
+            max_score = score
+            best_hand = hand
+
+    return best_hand, max_score
 
 
 def score_discard(hand: list[Card], discard: list[Card]):
-    deck = [Card(value, suit) for value in values for suit in suits]
+    deck = [Card(rank, suit) for rank in ranks for suit in suits]
     pass
 
 
@@ -40,7 +103,7 @@ def find_best_hand(hand: list[Card]) -> list[Card]:
 
 
 suits = ("♠", "♥", "♦", "♣")
-values = [
+ranks = [
     "A",
     "2",
     "3",
@@ -56,7 +119,8 @@ values = [
     "K",
 ]
 
-deck = [Card(value, suit) for value in values for suit in suits]
+
+deck = [Card(rank, suit) for rank in ranks for suit in suits]
 
 dealt_pool = sample(deck, k=13)
 hand_a = dealt_pool[0:11:2]
